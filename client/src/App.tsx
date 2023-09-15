55bfb55027fdb6808774a3213e20629dbceb4753
conorv <70652079+conorvenus@users.noreply.github.com>
2023-09-15 15:21:15 +0100

    Added the ability to have multiple choice questions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
+import MultipleChoice from "./components/MultipleChoice"
+
 function App() {
     return (
-        <h1>hello</h1>
+        <MultipleChoice question={{
+            "question": "What is the binary representation of the decimal number 12?",
+            "correctAnswer": "1100",
+            "incorrectAnswer1": "1001",
+            "incorrectAnswer2": "1010",
+            "incorrectAnswer3": "1111"
+        }} questionNumber={1} numberOfQuestions={4} />
     )
 }
 
