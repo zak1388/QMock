6c75895da2cf93021b393dd5cce79fc61db9c8cc
Zakariyya Kadri <zakariyyakadri@gmail.com>
2023-09-17 19:17:22 +0100

    replace server call with data gotten previously

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,22 +14,30 @@ function App() {
     const [questions, setQuestions] = useState<QuestionType[]>([])
 
     async function getQuestionsByTopic(topic: ExtendedTopic) {
-        fetchEventSource("/api/questions", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify([topic]),
-            onmessage: (msg: any) => {
-                const TopicQuestionsSchema = z.object({
-                    topic: z.string(),
-                    questions: z.array(QuestionTypeSchema)
-                })
-                type TopicQuestions = z.infer<typeof TopicQuestionsSchema>
-                const data: TopicQuestions = JSON.parse(msg.data)
-                setQuestions(cur => [...cur, ...data.questions])
-            }
+        //fetchEventSource("/api/questions", {
+            //method: "POST",
+            //headers: {
+                //"Content-Type": "application/json"
+            //},
+            //body: JSON.stringify([topic]),
+            //onmessage: (msg: any) => {
+                //const TopicQuestionsSchema = z.object({
+                    //topic: z.string(),
+                    //questions: z.array(QuestionTypeSchema)
+                //})
+                //type TopicQuestions = z.infer<typeof TopicQuestionsSchema>
+                //const data: TopicQuestions = JSON.parse(msg.data)
+                //setQuestions(cur => [...cur, ...data.questions])
+            //}
+        //})
+        const TopicQuestionsSchema = z.object({
+            topic: z.string(),
+            questions: z.array(QuestionTypeSchema)
         })
+        type TopicQuestions = z.infer<typeof TopicQuestionsSchema>
+        const payload = ` {"topic":"Test Topic","questions":[{"question":"Question 1: What is the most abundant gas in the Earth's atmosphere?","correctAnswer":"Nitrogen","incorrectAnswer1":"Oxygen","incorrectAnswer2":"Carbon dioxide","incorrectAnswer3":"Hydrogen"},{"question":"Question 2: Who developed the theory of relativity?","correctAnswer":"Albert Einstein","incorrectAnswer1":"Isaac Newton","incorrectAnswer2":"Galileo Galilei","incorrectAnswer3":"Marie Curie"},{"trueStatement":"The topic of Test Topic is highly complex and requires advanced knowledge to fully understand.","falseStatement":"The topic of Test Topic is very simple and can be easily understood by anyone."},{"trueStatement":"Studying Test Topic requires extensive research and analysis of various case studies in order to gain a deep understanding.","falseStatement":"Studying Test Topic can be done by reading a single book and does not require any additional research or analysis."},{"prompt":"Match the programming languages with their paradigms:","context":"Test Topic: test topic","pairs":[{"left":"Java","right":"Object-Oriented"},{"left":"Python","right":"Multi-Paradigm"},{"left":"Haskell","right":"Functional"},{"left":"SQL","right":"Query Language"}]}]}`
+        const data: TopicQuestions = JSON.parse(payload)
+        setQuestions(cur => [...cur, ...data.questions])
     }
 
     return (
